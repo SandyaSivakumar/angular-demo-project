1170e8e7120292f9c8704efc8747c4b8cd74a0a3
Sandya Sivakumar <sandya.sandy.s@gmail.com>
2020-02-18 15:28:13 +0530

    header and search bar modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
@@ -8,6 +8,8 @@ import { UserItemComponent } from './user-item/user-item.component';
 import { UserListComponent } from './user-list/user-list.component';
 import { FormElementComponent } from './form-element/form-element.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ResultTableComponent } from './result-table/result-table.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -16,11 +18,14 @@ import { NavbarComponent } from './navbar/navbar.component';
     UserItemComponent,
     UserListComponent,
     FormElementComponent,
-    NavbarComponent
+    NavbarComponent,
+    ResultTableComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
