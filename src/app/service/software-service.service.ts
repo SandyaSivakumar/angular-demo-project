192ba51538232ea1e19f79d3aef31ac477d1ae12
Sandya Sivakumar <sandya.sandy.s@gmail.com>
2020-02-20 10:37:43 +0530

    connecting springboot and angular

--- src/app/service/software-service.service.ts
+++ src/app/service/software-service.service.ts
@@ -1,18 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Software } from '../model/software';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SoftwareServiceService {
-  private swUrl: string;
+  public swUrl: string;
+  public http: HttpClient;
   constructor() {
     this.swUrl = "http://localhost:8080/software";
    }
 
   public findAll(): Observable<Software[]> {
+      console.log(this.swUrl);
       return this.http.get<Software[]>(this.swUrl);
     }
 }
+
+
+
+
