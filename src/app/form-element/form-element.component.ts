a30d568c43d1fd9dd6548c82c7d624c74c1e6582
Sandya Sivakumar <sandya.sandy.s@gmail.com>
2020-03-02 09:12:05 +0530

    Analytic charts section

--- src/app/form-element/form-element.component.ts
+++ src/app/form-element/form-element.component.ts
@@ -2,19 +2,30 @@ import { Component, OnInit,Input } from '@angular/core';
 import {MatButton} from '@angular/material/button';
 import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { PieChartComponent } from '../pie-chart/pie-chart.component';
+import { StackedChartComponent } from '../stacked-chart/stacked-chart.component';
 
 @Component({
   selector: 'ngbd-modal-content',
   template: `
     <div class="modal-header">
-      <h4 class="modal-title">Hi there!</h4>
+      <h4 class="modal-title">Some Analytics on Software Data</h4>
       <button type="button" class="close" aria-label="Close" (click)="activeModal.dismiss('Cross click')">
         <span aria-hidden="true">&times;</span>
       </button>
     </div>
     <div class="modal-body">
-      <p>Hello, {{name}}!</p>
-      <app-pie-chart></app-pie-chart>
+      <table>
+        <tr>
+        <td><app-pie-chart></app-pie-chart></td>
+        <td>The pie-chart represents the distribution of the languages used in the complete list of softwares.
+        This is measured through the lines of code parameter which is associated with each software.</td>
+        </tr>
+        <tr>
+        <td><app-stacked-chart></app-stacked-chart></td>
+        <td>The stacked bar graph represents the distribution of the languages used in each application.
+        The applications include CD Data Plane, Continuous Delivery Experience, CD Analytics</td>
+        </tr>
+      </table>
     </div>
     <div class="modal-footer">
       <button type="button" class="btn btn-outline-dark" (click)="activeModal.close('Close click')">Close</button>
@@ -35,7 +46,7 @@ export class FormElementComponent {
   constructor(private modalService: NgbModal) {}
 
   open() {
-    const modalRef = this.modalService.open(NgbdModalContent);
+    const modalRef = this.modalService.open(NgbdModalContent,{ size: 'xl' });
     modalRef.componentInstance.name = 'World';
   }
 }
--- src/app/stacked-chart/stacked-chart.component.ts
+++ src/app/stacked-chart/stacked-chart.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
+import { Label } from 'ng2-charts';
 
 @Component({
   selector: 'app-stacked-chart',
   templateUrl: './stacked-chart.component.html',
-  styleUrls: ['./stacked-chart.component.css']
+  styleUrls: [ './stacked-chart.component.html' ]
 })
-export class StackedChartComponent implements OnInit {
+export class StackedChartComponent  {
+  public barChartOptions: ChartOptions = {
+    responsive: true,
+  };
+  public barChartLabels: Label[] = ['CDP', 'CDE', 'CDA'];
+  public barChartType: ChartType = 'bar';
+  public barChartLegend = true;
+  public barChartPlugins = [];
+
+    public barChartData: ChartDataSets[] = [
+      { data: [3200,0,6698], label: 'Java', stack: 'a' },
+      { data: [1115,0,1700], label: 'Python', stack: 'a' },
+      { data: [4736,0,0], label: 'Go', stack: 'a' },
+      { data: [0,5600,0], label: 'TypeScript', stack: 'a' }
+    ];
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
-
 }
